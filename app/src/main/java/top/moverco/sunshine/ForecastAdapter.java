4288d34a366eed4af668aa48f87e695fde5aa06f
Moverco <moverco@163.com>
2016-12-21 15:59:28 +0800

    0.03 >change gradle file to fix compile error

--- app/src/main/java/top/moverco/sunshine/ForecastAdapter.java
+++ app/src/main/java/top/moverco/sunshine/ForecastAdapter.java
@@ -1,27 +1,34 @@
 package top.moverco.sunshine;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-
 /**
  * Created by liuzongxiang on 2016/12/20.
  */
 
 public class ForecastAdapter extends BaseAdapter {
     List<ForecastBean> forecastItem = new ArrayList<ForecastBean>();
+    LayoutInflater layoutInflater;
+    ForecastAdapter(Context mContext){
+        this.layoutInflater = LayoutInflater.from(mContext);
+    }
+
     @Override
     public int getCount() {
         return forecastItem.size();
     }
 
     @Override
-    public Object getItem(int position) {
-        return (ForecastBean)forecastItem.get(position);
+    public ForecastBean getItem(int position) {
+        return forecastItem.get(position);
     }
 
     @Override
@@ -31,9 +38,15 @@ public class ForecastAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ForecastBean forecastItem = (ForecastBean) getItem(position);
+        ViewHolder viewHolder;
+        if(convertView==null){
+
+        }
 
 
-
+    }
+     class ViewHolder {
+        TextView date_view,weather_view,hi_tem_view,lo_tem_view;
+        ImageView weather_image_view;
     }
 }
--- app/src/main/java/top/moverco/sunshine/ListFragment.java
+++ app/src/main/java/top/moverco/sunshine/ListFragment.java
@@ -2,7 +2,6 @@ package top.moverco.sunshine;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +11,7 @@ import android.view.ViewGroup;
  */
 
 public class ListFragment extends Fragment {
-    @Nullable
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
