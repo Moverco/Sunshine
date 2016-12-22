c8af537934d4ebfd83028da4c330ae9efbaafe97
Moverco <moverco@163.com>
2016-12-22 17:13:09 +0800

    0.05 >Add AsyncTask&HttpURLConnection to adding new function.

--- app/src/main/java/top/moverco/sunshine/ListFragment.java
+++ app/src/main/java/top/moverco/sunshine/ListFragment.java
@@ -1,10 +1,15 @@
 package top.moverco.sunshine;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by liuzongxiang on 2016/12/20.
@@ -18,6 +23,29 @@ public class ListFragment extends Fragment {
         View view = inflater.inflate(R.layout.listfragment,container,false);
         return view;
 
+    }
+    private class Task extends AsyncTask<URL,Integer,Long>{
+
+        @Override
+        protected Long doInBackground(URL... params) {
+            HttpURLConnection httpURLConnection = new HttpURLConnection() {
+                @Override
+                public void disconnect() {
+
+                }
+
+                @Override
+                public boolean usingProxy() {
+                    return false;
+                }
+
+                @Override
+                public void connect() throws IOException {
+
+                }
+            }
+            return null;
+        }
     }
 
 }
